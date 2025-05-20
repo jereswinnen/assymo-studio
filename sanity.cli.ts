4a914ec898542192fd18f958a9ab40cf6cf88f20
Jeremy Swinnen <hey@jeremys.be>
2025-05-20 12:51:01 +0200

    Updated sanity cli file with studioHost

--- sanity.cli.ts
+++ sanity.cli.ts
@@ -3,8 +3,9 @@ import {defineCliConfig} from 'sanity/cli'
 export default defineCliConfig({
   api: {
     projectId: 'naj44gzh',
-    dataset: 'production'
+    dataset: 'production',
   },
+  studioHost: 'assymo',
   /**
    * Enable auto-updates for studios.
    * Learn more at https://www.sanity.io/docs/cli#auto-updates
